cd70483b2812797396be882b0f8c2f0f88a40f4b
andimags <andimagsumbol@gmail.com>
2026-01-28 17:01:07 +0800

    feat: add login function on auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/user.dto';
 import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private userService: UserService) {}
+    constructor(
+        private readonly userService: UserService,
+        private readonly authService: AuthService
+    ) {}
 
     @Post('register')
     async register(@Body() dto: CreateUserDto) {
         return await this.userService.createUser(dto);
+    }
+
+    @Post('login')
+    async login(@Body() dto: LoginDto){
+        return await this.authService.login(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+        constructor(
+            private readonly prisma: PrismaService, 
+            private readonly userService: UserService,
+            private readonly jwtService: JwtService
+        ) {}
+
+        async login(dto: LoginDto){
+            const user = await this.userService.user({
+                email: dto.email
+            })
+
+            if(user && await compare(dto.password, user.password)){
+                const { password: _password, ...result } = user;
+
+                return {
+                    user: result,
+                    backendTokens: {
+                        accessToken: await this.jwtService.signAsync(result, {
+                            expiresIn: '1hr',
+                            secret: process.env.JWT_ACCESS_KEY
+                        }),
+                        refreshToken: await this.jwtService.signAsync(result, {
+                            expiresIn: '7d',
+                            secret: process.env.JWT_REFRESH_KEY
+                        })
+                    }
+                }
+            }
+
+            throw new UnauthorizedException;
+        }
+}
